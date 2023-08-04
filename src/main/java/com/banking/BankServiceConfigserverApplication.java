e02e33331730a26cd6827fcdd0d4d6704afeff4d
theanuragharsh <nrgharsh@gmail.com>
2023-08-05 00:40:59 +0530

    Added env-config files

--- src/main/java/com/banking/BankServiceConfigserverApplication.java
+++ src/main/java/com/banking/BankServiceConfigserverApplication.java
@@ -2,12 +2,14 @@ package com.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BankServiceConfigserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankServiceConfigserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceConfigserverApplication.class, args);
+    }
 
 }
